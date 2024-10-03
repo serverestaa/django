4664113c6a28592bee4fde09f67c86eb4ea0079c
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-03 21:50:50 +0500

    Adding payments via Stripe and display of an order on Stripe's payment page

--- products/admin.py
+++ products/admin.py
@@ -9,7 +9,7 @@ admin.site.register(ProductCategory)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','price','quantity','category')
-    fields = ('name','description',('price','quantity'),'image','category')
+    fields = ('name','description',('price','quantity'),'stripe_product_price_id','image','category')
     readonly_fields = ('description',)
     search_fields = ('name',)
     ordering = ('name',)
