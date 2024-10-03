2747db650b1eb8337bcde76669f8323893bf617d
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-04 00:45:45 +0500

    Added orders view button and details of each order + fixed bug with item quantity

--- orders/views.py
+++ orders/views.py
@@ -28,6 +28,27 @@ class CanceledTemplateView(TemplateView):
     template_name = 'orders/canceled.html'
 
 
+class OrderListView(TitleMixin, ListView):
+    template_name = 'orders/orders.html'
+    title = 'Store - Заказы'
+    queryset = Order.objects.all()
+    ordering = ('-id')
+
+    def get_querysqet(self):
+        queryset = super(OrderListView, self).get_queryset()
+        return queryset.fiter(initiator=self.request.user)
+
+
+class OrderDetailView(DetailView):
+    template_name = 'orders/order.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderDetailView, self).get_context_data(**kwargs)
+        context['title'] = f'Store - Заказ #{self.object.id}'
+        return context
+
+
 class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderForm
@@ -82,4 +103,3 @@ def stripe_webhook_view(request):
 
     # Passed signature verification
     return HttpResponse(status=200)
-
