8e6a8f4e2d7eb8a3a220576c648e63080a30d4e8
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-04 17:03:36 +0500

    improve of ProductModelView

--- api/views.py
+++ api/views.py
@@ -1,8 +1,7 @@
-from rest_framework.generics import ListAPIView
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework.viewsets import ModelViewSet
 
-
-class ProductListAPIView(ListAPIView):
+class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
