c0ae3bd0e127d84f0d0a21ad1a1032655a811881
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-04 16:27:29 +0500

    fixed category in product list API

--- products/serializers.py
+++ products/serializers.py
@@ -3,6 +3,7 @@ from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
